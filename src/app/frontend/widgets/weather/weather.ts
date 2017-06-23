ade83c63a9dfb2f0012fc7f0deab12c239c2e14a
kschima <kschima@users.noreply.github.com>
2017-06-23 13:20:06 +0200

    Some UI changes

--- src/app/frontend/widgets/weather/weather.ts
+++ src/app/frontend/widgets/weather/weather.ts
@@ -2,9 +2,24 @@ export class WeatherCurrent{
   constructor(
     public city: string,
     public description: string,
-    public temperature: number){
+    public temperature: number,
+    public time: number){
     this.city = city;
     this.description = description;
     this.temperature = temperature;
+    this.time = time;
   }
 }
+
+export class WeatherForecast{
+  constructor(
+    public description: string,
+    public temperature: number,
+    public time: number){
+    this.description = description;
+    this.temperature = temperature;
+    this.time = time;
+  }
+}
+
+
