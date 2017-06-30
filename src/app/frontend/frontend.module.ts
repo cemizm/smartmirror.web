dac3776255790c932e1dda5eced94589664506dd
MyAhz <sokemad@hotmail.com>
2017-06-30 15:45:04 +0200

    Change device linking responsibility

--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -2,14 +2,10 @@ import {NgModule} from "@angular/core";
 import {SharedModule} from "../shared/shared.module";
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
-import {TicketComponent} from "./ticket/ticket.component";
 import {WidgetsModule} from "./widgets/widgets.module";
-import {InitComponent} from "./init/init.component";
 
 export const ModuleRoutes: Routes = [
-  {path: '', component: InitComponent},
-  {path: 'ticket', component: TicketComponent},
-  {path: 'home', component: HomeComponent}
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
@@ -18,7 +14,7 @@ export const ModuleRoutes: Routes = [
     RouterModule.forChild(ModuleRoutes),
     WidgetsModule
   ],
-  declarations: [HomeComponent, TicketComponent, InitComponent]
+  declarations: [HomeComponent]
 })
 export class FrontendModule {
 }
--- src/app/frontend/home/home.component.ts
+++ src/app/frontend/home/home.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from "@angular/core";
+import {Mirror, MirrorService, Ticket, TicketService} from "@cemizm/smartmirror-shared";
+import {MirrorSettingService} from "../../shared/services/mirror-setting.service";
 
 @Component({
   selector: 'app-home',
@@ -7,12 +9,24 @@ import {Component, OnInit} from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  private ticket: Ticket;
+  private mirror: Mirror;
+
+  constructor(private ms: MirrorService, private mss: MirrorSettingService, private ts: TicketService) {
   }
 
 
   ngOnInit() {
-
+    this.ms.getById(this.mss.getId()).subscribe(mirror => {
+        this.mirror = mirror;
+      },
+      err => {
+        if (err.status === 404) {
+          this.ts.get(this.mss.getId()).subscribe(ticket => {
+            this.ticket = ticket;
+          });
+        }
+      });
   }
 
 }
