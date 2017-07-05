1b57448fd608e2adb7674e5123fd1c09ce94b0be
kschima <kschima@users.noreply.github.com>
2017-07-05 17:28:47 +0200

    Add missing widget types

--- src/app/frontend/home/home.component.ts
+++ src/app/frontend/home/home.component.ts
@@ -2,8 +2,11 @@ import {Component, OnInit} from "@angular/core";
 import {Mirror, MirrorService, Ticket, TicketService, Widget, WidgetSide, WidgetType} from "@cemizm/smartmirror-shared";
 import {MirrorSettingService} from "../../shared/services/mirror-setting.service";
 import {Observable} from "rxjs/Observable";
-import {WeathercurrentComponent} from "../widgets/weather/weathercurrent/weathercurrent.component";
 import {NewsViewComponent} from "../widgets/news/news-view/news-view.component";
+import {CalendarViewComponent} from "../widgets/calendar/calendar-view/calendar-view.component";
+import {MailsComponent} from "../widgets/mails/mails.component";
+import {NoteComponent} from "../widgets/note/note.component";
+import {WeatherpreviewComponent} from "../widgets/weather/weatherpreview/weatherpreview.component";
 
 @Component({
   selector: 'app-home',
@@ -14,16 +17,9 @@ export class HomeComponent implements OnInit {
 
   private ticket: Ticket;
   private mirror: Mirror;
-  private rightSide: number;
-  private widgetsLeft: Observable<Array<Widget>>;
-  private widgetsRight: Observable<Array<Widget>>;
-
 
   constructor(private ms: MirrorService, private mss: MirrorSettingService, private ts: TicketService) {
-  this.rightSide = WidgetSide.Right;
-
   }
-
 
   ngOnInit() {
     this.ms.getById(this.mss.getId()).subscribe(mirror => {
@@ -41,9 +37,15 @@ export class HomeComponent implements OnInit {
   load(widget: Widget) {
     switch (widget.type) {
       case WidgetType.Weather:
-        return WeathercurrentComponent;
+        return WeatherpreviewComponent;
+      case WidgetType.Calender:
+        return CalendarViewComponent;
       case WidgetType.News:
         return NewsViewComponent;
+      case WidgetType.Mails:
+        return MailsComponent;
+      case WidgetType.Notes:
+        return NoteComponent;
     }
   }
 
