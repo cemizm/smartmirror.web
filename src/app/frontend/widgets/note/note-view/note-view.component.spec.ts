1e4a7430e4c6be053b70e1526ccb494b2511d892
MyAhz <sokemad@hotmail.com>
2017-07-09 17:21:05 +0200

    Add data handling to Note Widget

--- src/app/frontend/widgets/note/note-view/note-view.component.spec.ts
+++ src/app/frontend/widgets/note/note-view/note-view.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NoteComponent } from './note.component';
+import { NoteViewComponent } from './note-view.component';
 
-describe('NoteComponent', () => {
-  let component: NoteComponent;
-  let fixture: ComponentFixture<NoteComponent>;
+describe('NoteViewComponent', () => {
+  let component: NoteViewComponent;
+  let fixture: ComponentFixture<NoteViewComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NoteComponent ]
+      declarations: [ NoteViewComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NoteComponent);
+    fixture = TestBed.createComponent(NoteViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
