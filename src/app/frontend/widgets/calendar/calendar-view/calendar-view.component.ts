9bcaa9d87db64c03325059f12e42236302489f61
Cem Basoglu <cem.basoglu@web.de>
2017-07-12 08:40:33 +0200

    calendar events

--- src/app/frontend/widgets/calendar/calendar-view/calendar-view.component.ts
+++ src/app/frontend/widgets/calendar/calendar-view/calendar-view.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from "@angular/core";
-import {CalendarSettings, Event} from "@cemizm/smartmirror-shared";
-import {CalendarService} from "../calendar.service";
+import {CalendarSettings, Event, EventsService} from "@cemizm/smartmirror-shared";
+import {Observable} from "rxjs";
+
+const UPDATE_INTERVAL = 1000 * 60 * 0.5;
 
 @Component({
   selector: 'app-calendar-view',
@@ -12,10 +14,21 @@ export class CalendarViewComponent implements OnInit {
 
   @Input() setting: CalendarSettings | CalendarSettings;
 
-  constructor(private calendarService: CalendarService) {
+  constructor(private eventsService: EventsService) {
   }
 
   ngOnInit() {
-    this.calendarService.calEventListSubject.subscribe(data => this.calEventList = data);
+    Observable.timer(0, UPDATE_INTERVAL).subscribe(() => this.update());
+  }
+
+  update() {
+    this.eventsService.list(this.setting.oAuthToken, this.setting.calendarId,
+      {
+        orderBy: "startTime",
+        singleEvents: true,
+        timeMin: new Date().toISOString()
+      }).subscribe(events => {
+      this.calEventList = events;
+    });
   }
 }
