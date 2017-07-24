236c580af6f9eaafd89fb407dc7efdf94d6cff78
Cem Basoglu <cem.basoglu@web.de>
2017-07-24 13:14:10 +0200

    mail ui polish

--- src/app/frontend/widgets/mails/mail-view/mail-view.component.ts
+++ src/app/frontend/widgets/mails/mail-view/mail-view.component.ts
@@ -1,9 +1,16 @@
 import {Component, Input, OnInit} from "@angular/core";
-import {MailSettings, Message} from "@cemizm/smartmirror-shared";
+import {MailSettings} from "@cemizm/smartmirror-shared";
 import {Observable} from "rxjs/Observable";
 import {MailService} from "../mail.service";
 
 const interval = 5000;
+
+interface MailItem {
+  subject: string;
+  from: string;
+  date: Date;
+  snippet: string;
+}
 
 @Component({
   selector: 'app-mail-view',
@@ -12,7 +19,7 @@ const interval = 5000;
 })
 export class MailViewComponent implements OnInit {
   @Input() setting: MailSettings | MailSettings;
-  private mailList: Array<Message>;
+  private mails: Array<MailItem>;
 
   constructor(private mailService: MailService) {
   }
@@ -25,6 +32,37 @@ export class MailViewComponent implements OnInit {
     if (!this.setting || !this.setting.oAuthToken) {
       return;
     }
-    this.mailService.getMails(this.setting.oAuthToken, this.setting.maxCount).subscribe(mails => this.mailList = mails);
+    this.mailService.getMails(this.setting.oAuthToken, this.setting.maxCount).subscribe(mails => {
+      this.mails = new Array<MailItem>();
+
+      for (const item of mails) {
+        let subject: string = null;
+        let from: string = null;
+        let date: Date = null;
+
+        for (const head of item.payload.headers) {
+          if (head.name == "Date")
+            date = new Date(head.value);
+
+          if (head.name == "Subject")
+            subject = head.value;
+
+          if (head.name == "From")
+            from = head.value;
+        }
+
+        if (from && from.indexOf("<") > 0) {
+          from = from.substr(0, from.indexOf("<"));
+        }
+
+        this.mails.push({
+          subject,
+          from,
+          date,
+          snippet: item.snippet,
+        });
+      }
+
+    });
   }
 }
