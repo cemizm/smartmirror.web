15718d9e5f843724c7d2d7e7ca874af7f9a96ef6
Cem Basoglu <cem.basoglu@web.de>
2017-07-25 11:21:59 +0200

    refactor news and unsubscribe

--- src/app/frontend/widgets/news/news-view/news-view.component.ts
+++ src/app/frontend/widgets/news/news-view/news-view.component.ts
@@ -1,34 +1,40 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, Input, OnInit, OnDestroy} from "@angular/core";
 import {NewsService} from "../news.service";
 import {FeedRSS} from "../news.models";
 import {NewsSetting} from "@cemizm/smartmirror-shared";
+import {Observable, Subscription} from "rxjs";
+
+const interval = 1000 * 60 * 1;
 
 @Component({
   selector: 'app-news-view',
   templateUrl: './news-view.component.html',
   styleUrls: ['./news-view.component.scss']
 })
-export class NewsViewComponent implements OnInit {
+export class NewsViewComponent implements OnDestroy, OnInit {
 
   private feedRss: FeedRSS;
+  private sub: Subscription;
+
   @Input() setting: NewsSetting | NewsSetting;
 
   constructor(private newsService: NewsService) {
   }
 
   ngOnInit() {
-    this.getFeed();
+    this.sub = Observable.timer(0, interval).subscribe(() => this.update());
   }
 
-  getFeed() {
-    this.newsService.getFeedContent(this.setting.feedUrl).subscribe(data => {
-      this.feedRss = data;
-      this.feedRss.items.splice(this.setting.maxCount);
-    });
+  ngOnDestroy() {
+    if (this.sub)
+      this.sub.unsubscribe();
   }
 
-  getpolledFeed() {
-    this.newsService.polledHttpGetRequest(this.setting.feedUrl).subscribe(data => {
+  update() {
+    if (!this.setting || !this.setting.feedUrl) {
+      return;
+    }
+    this.newsService.getFeed(this.setting.feedUrl).subscribe(data => {
       this.feedRss = data;
       this.feedRss.items.splice(this.setting.maxCount);
     });
--- src/app/frontend/widgets/news/news.service.ts
+++ src/app/frontend/widgets/news/news.service.ts
@@ -3,30 +3,30 @@ import {Http} from "@angular/http";
 import {Observable} from "rxjs/Rx";
 import {FeedRSS} from "./news.models";
 import "rxjs/add/observable/interval";
+import * as moment from "moment";
 
 
 @Injectable()
 export class NewsService {
-  private rssToJsonServiceBaseUrl = 'https://api.rss2json.com/v1/api.json';
-  private interval = 1000 * 60 * 30;
+  private url = 'https://api.rss2json.com/v1/api.json';
 
   constructor(private http: Http) {
   }
 
-  getFeedContent(url: string): Observable<FeedRSS> {
-    return this.http.get(this.rssToJsonServiceBaseUrl, {
+  getFeed(url: string): Observable<FeedRSS> {
+    return this.http.get(this.url, {
       params: {
-        'rss_url': url
+        'rss_url': url,
+        "api_key": 'xtlvhz8u6yjakvlvuwkcggj80kwpmidajouefmpu'
       }
-    }).map(res => <FeedRSS>res.json());
-  }
-
-  polledHttpGetRequest(url: string): Observable<FeedRSS> {
-    return Observable.interval(this.interval)
-      .switchMap(() => this.http.get(this.rssToJsonServiceBaseUrl, {
-        params: {
-          'rss_url': url
+    }).map(res => <FeedRSS>res.json())
+      .map(feed=> {
+        for (const item of feed.items) {
+          if (item.pubDate)
+            item.pubDate = moment(item.pubDate).add(2, "hour").toDate();
         }
-      }).map(res => <FeedRSS>res.json()));
+
+        return feed;
+      });
   }
 }
